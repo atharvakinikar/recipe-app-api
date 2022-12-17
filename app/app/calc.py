d30d351a3409f6d194ba07bed76c50ba82610216
Atharva <atharvapradeeprao.k20@iiits.in>
2022-12-17 20:26:50 +0530

    Added psycopg2

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,10 +2,11 @@
 Calculator Function
 """
 
-def add(x,y):
-    """Add x and y and return"""
-    return x+y
 
-def subtract(x,y):
+def add(x, y):
+    """Add x and y and return"""
+    return x + y
+
+def subtract(x, y):
     """Subtract x from y and return result"""
-    return y-x
+    return y - x
